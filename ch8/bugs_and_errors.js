5fb5493683094c338f9538ddb692f024905ad22f
Zach Behnke <ztbehnke62@gmail.com>
2022-06-20 20:28:50 -0400

    Chapter 8 exercises. Bugs and errors.

---
+++ ch8/bugs_and_errors.js
@@ -0,0 +1,56 @@
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+  try {
+    return primitiveMultiply(a, b);
+  } catch (e) {
+    if ( e instanceof MultiplicatorUnitFailure) return reliableMultiply(a, b);
+  }
+}
+
+//console.log(reliableMultiply(8, 8));
+
+const box = {
+  locked: true,
+  unlock() { this.locked = false; },
+  lock() { this.locked = true;  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  }
+};
+
+function withBoxUnlocked(body) {
+  const boxState = box.locked;
+  box.unlock();
+  try {
+    return body();
+  } finally {
+    if (boxState) {
+      box.lock();
+    }
+  }
+}
+
+withBoxUnlocked(function() {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function() {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised: " + e);
+}
+console.log(box.locked);
+// → true
